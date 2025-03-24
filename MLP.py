d18bbaed06e8fe7487b71e5293312410cc7887c5
vitordossantos <vitorsousa@poli.ufrj.br>
2025-03-23 23:24:40 -0300

    MLP.py

--- MLP.py
+++ MLP.py
@@ -60,12 +60,21 @@ The next line in effect selects the input features that the ML model will use to
 df_allEta = df_allEta[['lead_jet_pt','lead_jet_eta','lead_jet_phi','trail_jet_pt','trail_jet_eta','trail_jet_phi','third_jet_pt','third_jet_eta','third_jet_phi','leading_lep_pt','leading_lep_eta','leading_lep_phi','trailing_lep_pt','trailing_lep_eta','trailing_lep_phi','met_pt','met_phi','dijet_mass','dijet_deta','dilep_m','ngood_jets','final_weight','isSignal',\
                        'proc']] 
 
-df_allEta.loc[df_allEta['ngood_jets'] > 2,'ngood_jets'] = 3 # This line is in charge of setting the value 3 for the number of good jets, in the events that have more than 2. This is done this way because Yacine told me that we can not trust the ngood_jets variable to tell exactly the number of good jets the event has, but we can trust the information that the event has more than 2 every time we have the ngood_jets variable greater than 2
+'''
+This next line is in charge of setting the value 3 for the number of good jets, in the events that have more than 2. This is done this way because Yacine told me that we can not trust the ngood_jets
+variable to tell exactly the number of good jets the event has, but we can trust the information that the event has more than 2, every time we have the ngood_jets variable greater than 2
+'''
+df_allEta.loc[df_allEta['ngood_jets'] > 2,'ngood_jets'] = 3 
 
-df_allEta.loc[df_allEta['ngood_jets'] == 2,'third_jet_pt'] = 30.0 # This is important to give this quantity a value different than -99 every time the event has only 2 jets: since we must format the dataframe to contain all the events, no matter the number of good jets they have, every time the event has only 2 good jets its -99 value squeezes the at least 3 jets events information to a very small region in the 0.0 to 1.0 interval. This is done as a pre-processing procedure to enhance the MinMaxScalling
+'''
+The next 3 lines are important to give those quantities a value different than -99, every time the event has only 2 jets: since we must format the dataframe to contain all the events, no matter the number of good jets
+they have, every time the event has only 2 good jets its -99 value squeezes the at least 3 jets events information to a very small region in the 0.0 to 1.0 interval. This is done as a pre-processing procedure to enhance
+the MinMaxScalling.
+'''
+df_allEta.loc[df_allEta['ngood_jets'] == 2,'third_jet_pt'] = 30.0 # This is done as a pre-processing procedure to enhance the MinMaxScalling, the original value is -99 and prejudices MinMaxScalling
 
-df_allEta.loc[df_allEta['ngood_jets'] == 2,'third_jet_eta'] = 0.0 # This is done with the same purpose: enhance MinMaxScalling of the feature
-df_allEta.loc[df_allEta['ngood_jets'] == 2,'third_jet_phi'] = 0.0 # This is done with the same purpose: enhance MinMaxScalling of the feature
+df_allEta.loc[df_allEta['ngood_jets'] == 2,'third_jet_eta'] = 0.0 # This is done with the same purpose: enhance MinMaxScalling of the feature, the original value is -99 and prejudices MinMaxScalling
+df_allEta.loc[df_allEta['ngood_jets'] == 2,'third_jet_phi'] = 0.0 # This is done with the same purpose: enhance MinMaxScalling of the feature, the original value is -99 and prejudices MinMaxScalling
 
 bkgSumWeight = df_allEta.loc[df_allEta['isSignal'] == bkgLabel ,'final_weight'].sum() # Expresses the number of background events of the Run II 2018
 sigSumWeight = df_allEta.loc[df_allEta['isSignal'] == 1.0 ,'final_weight'].sum() # Expresses the number of NLO VBS, with final state in 2l 2nu events, for the Run II 2018
@@ -75,7 +84,10 @@ df_allEta.loc[df_allEta['isSignal'] == 1.0 ,'final_weight'] /= sigSumWeight # Th
 
 from sklearn.preprocessing import MinMaxScaler
 
-dataset = df_allEta.iloc[:,0:21].to_numpy() # This line gets the values from the lead_jet_pt to ngood_jets parameters, they will be used as input features for the MLP, the ones it can rely upon to separate signal from background
+'''
+The next line gets the values from the lead_jet_pt to ngood_jets parameters, they will be used as input features for the MLP, the ones it can rely upon to separate signal from background
+'''
+dataset = df_allEta.iloc[:,0:21].to_numpy()
 dataset_W = df_allEta.iloc[:,21].to_numpy() # Defines a numpy array that represent normalized events weights, doesn't care if it's a background or a signal event
 dataset_Y = df_allEta.iloc[:,22].to_numpy() # Defines the numpy array that stands for the targets of the events
 scaler = MinMaxScaler() # Defines a MinMaxScaller
@@ -94,8 +106,8 @@ from keras.callbacks import LearningRateScheduler, ModelCheckpoint, Callback
 filepath = data_dir3+'Unified/' # just a procedure to align the filepath of the model checkpoint class to a target folder in google drive
 
 '''
-The following class definition is the custom significance class I use to choose the best training epoch, by selecting the highest validation point, as well as an agreement of less than 1% between its values for the training and 
-validation datasets. I've also used it to understand if a MLP model is already very huge and unnecessary for the task of separating VBS from background
+The following class definition is the custom significance class I use to choose the best training epoch, by selecting the highest validation point, as well as an agreement of less than 1% between its values for the 
+training and validation datasets. I've also used it to understand if a MLP model is already very huge and unnecessary for the task of separating VBS from background
 '''
 @tf.keras.utils.register_keras_serializable()
 class globalSignificance(tf.keras.metrics.Metric):
@@ -134,15 +146,15 @@ class globalSignificance(tf.keras.metrics.Metric):
       isTotalSig = tf.multiply(isTotalSig, sample_weight) # Normalized vector of weights of all the signal events in the batch
       isTotalBkg = tf.multiply(isTotalBkg, sample_weight) # Normalized vector of weights of all the background events in the batch
 
-    self.sigPart.assign_add(self.sigSumWeight*tf.reduce_sum(isSig)) # here I set the self.sigPart variable with the physical world number of true positive events in the current batch (I have to denormalize to calculate significance)
-    self.bkgPart.assign_add(self.bkgSumWeight*tf.reduce_sum(isBkg)) # here I set the self.bkgPart variable with the physical world number of false positive events in the current batch (I have to denormalize to calculate significance)
+    self.sigPart.assign_add(self.sigSumWeight*tf.reduce_sum(isSig)) # Set the self.sigPart variable with the physical world number of true positive events in the current batch (have to denormalize to calculate significance)
+    self.bkgPart.assign_add(self.bkgSumWeight*tf.reduce_sum(isBkg)) # Set the self.bkgPart variable with the physical world number of false positive events in the current batch (have to denormalize to calculate significance)
     self.sigPartTotal.assign_add(self.sigSumWeight*tf.reduce_sum(isTotalSig)) # the physical world number of signal events in the batch
     self.bkgPartTotal.assign_add(self.bkgSumWeight*tf.reduce_sum(isTotalBkg)) # the physical world number of background events in the batch
 
   '''
-  In the result method of the globalSignificance class I calculate the projected significance. Since I'am using only the 2018 MC it's important to scale it properly to predict how great the significance would be if I was using all the 
-  run II MC 1.513274 * [sqrt(2018 total number of events) / sqrt(total number of events assessed up to the current batch)] * [self.sigPart / sqrt(self.bkgPart)]. The value 1.513274 is due to the term sqrt(137.4/60), which is nothing
-  more than a scale factor to prospect how great the significancy would be if the model was applied to the entire run II.
+  In the result method of the globalSignificance class I calculate the projected significance. Since I'am using only the 2018 MC it's important to scale it properly to predict how great the significance would be if I was
+  using all the run II MC 1.513274 * [sqrt(2018 total number of events) / sqrt(total number of events assessed up to the current batch)] * [self.sigPart / sqrt(self.bkgPart)]. The value 1.513274 is due to the term
+  sqrt(137.4/60), which is nothing more than a scale factor to prospect how great the significancy would be if the model was applied to the entire run II.
   '''
   def result(self):
     return 1.513274*tf.math.divide(tf.math.multiply(tf.math.sqrt(self.sigSumWeight+self.bkgSumWeight),self.sigPart),\
@@ -186,8 +198,8 @@ class CustomModelCheckpoint(Callback):
         train_metric = logs.get(f'{self.monitor}', None) # # get the training set value for the globalSignificance custom metric class in the end of the training epoch
         current_val_loss = logs.get('val_loss') # Does the same thing but here the purpose is to get the validation loss
         '''
-        The next condition is designed to break the endless training loop in the main part of the code (which is defined outside this class, in the main code) whenever we reach the final training epoch and obtain a valid model.
-        There may be times when no model is obtained due to overly demanding conditions.
+        The next condition is designed to break the endless training loop in the main part of the code (which is defined outside this class, in the main code) whenever we reach the final training epoch and obtain a valid
+	model. There may be times when no model is obtained due to overly demanding conditions.
         '''
         if ((epoch == (self.trainingEpochs-1)) & (self.best_val_metric > self.minimum_acceptable_val_metric)):
           self.model.stop_training = True # This is done to stop the "while not self.model.stop_training" loop, in the main code, whenever we find a model that fulfills our demands 
@@ -195,11 +207,12 @@ class CustomModelCheckpoint(Callback):
 	''' 
         The next conditionals exists to promote the logic necessary to register a valid machine learning model, during the training process, rejecting any model without the preset required conditions.
         '''
-        if (self.mode == 'min' and (current_val_metric < self.best_val_metric)) or (self.mode == 'max' and (current_val_metric > self.best_val_metric)): # This conditional is true whenever the model reaches another high in validation
+        if (self.mode == 'min' and (current_val_metric < self.best_val_metric)) or (self.mode == 'max' and (current_val_metric > self.best_val_metric)): # True conditional whenever the model reaches another high in validation
             if train_metric is not None:  # If the training metric value is None there will be no computation!
                 if train_metric != np.nan: # This conditional is important to avoid numerical calculation errors.
                   '''
-                  The next line is important to compute the percentual deviation between the training and validation metric performances: "percent_diff" of the model must be lower than a predefined maximum for the model to be chosen.
+                  The next line is important to compute the percentual deviation between the training and validation metric performances: "percent_diff" of the model must be lower than a predefined maximum for the model
+		  to be chosen.
                   '''
                   percent_diff = ((current_val_metric - train_metric) / (train_metric)) * 100
 
@@ -207,10 +220,10 @@ class CustomModelCheckpoint(Callback):
                   percent_diff = np.inf # When the training metric is np.nan this line guarantees the model will not be chosen.
 
                 '''
-                The next conditional is in charge of registering a model that sets a new high in the validation "globalSig" metric. But will only be accepted if the "percent_diff is smaller than "self.trainValidation_percent_threshold.
-                In other words: no matter if we get a new high for the validation dataset. If the train dataset deviates too much from its performance, percentually, then the model will not be chosen. If the two mentioned conditions               
-                occur simultaneously, then a new best-performing model will be chosen and stored. It's noteworthy the relevance of the "self.maximum_acceptable_loss" parameter, even though I've always been setting it as "np.inf" and
-                , due to this, it has had no relevance or outcome in the model choosing process.
+                The next conditional is in charge of registering a model that sets a new high in the validation "globalSig" metric. But will only be accepted if the "percent_diff is smaller than
+		"self.trainValidation_percent_threshold. In other words: no matter if we get a new high for the validation dataset. If the train dataset deviates too much from its performance, percentually, then the model
+                will not be chosen. If the two mentioned conditions occur simultaneously, then a new best-performing model will be chosen and stored. It's noteworthy the relevance of the "self.maximum_acceptable_loss"
+		parameter, even though I've always been setting it as "np.inf" and, due to this, it has had no relevance or outcome in the model choosing process.
                 '''
                 if (percent_diff <= self.trainValidation_percent_threshold) & (current_val_loss < self.maximum_acceptable_loss):
                     self.best_val_metric = current_val_metric
@@ -259,20 +272,20 @@ while (not stopTraining):
     model.compile(optimizer=optim, loss='binary_crossentropy', weighted_metrics=[globalSig3Jets],run_eagerly=False)
 
     '''
-    The next line instantiates a CustomModelCheckpoint object to be passed to the model.fit method. As one can see in the next line, we set the preset requirements such as "minimum_acceptable_val_metric", which is the minimum acceptable
-    validation metric that a model must have to be chosen, the "trainValidation_percent_threshold" of 1%, which is the largest percentual deviation the validation metric value must have w.r.t the training metric value, for a model to be
-    chosen, the monitor is the "globalSig" custom metric and the mode is max, pointing out that we are searchig for the highest possible metric values. The number of training epochs is also important internally for the class, the name
-    of the custom metric class object and "filepath" are also important
+    The next line instantiates a CustomModelCheckpoint object to be passed to the model.fit method. As one can see in the next line, we set the preset requirements such as "minimum_acceptable_val_metric", which is the minimum
+    acceptable validation metric that a model must have to be chosen, the "trainValidation_percent_threshold" of 1%, which is the largest percentual deviation the validation metric value must have w.r.t the training metric
+    value, for a model to be chosen, the monitor is the "globalSig" custom metric and the mode is max, pointing out that we are searchig for the highest possible metric values. The number of training epochs is also important
+    internally for the class, the name of the custom metric class object and "filepath" are also important
     '''
     modelCheckPoint = CustomModelCheckpoint(globalSignif,filepath,monitor='globalSig',mode='max',save_best_only=True,trainValidation_percent_threshold = 1.0,trainingEpochs = epochs\
                                             ,minimum_accepted_val_metric = 1.9)
 
     '''
-    This next line sets the batch size, which I've prefired to set as a function of the number of batches that a dataset provides us. I've always procceed to prime factorize the "len(dataset_train)" quantity and try to set the
-    number of batches as the integer divisor of "len(dataset_train)" that lies as close as possible to 1000. This is due to the appearance of negative loss values, in some training epochs, when I use a very high number of batches. The
-    other issue that prevents me from using a batch size much larger than 1000 is the possibility of having bins with a negative number of events in the final histogram of the scores: this effect is due to the presence of events with
-    negative weights in the 2018 MC. At the same time if I use a number of batches much smaller than 1000 I suffer from the problem of impairing the generalization capabilities of the model as well as taking the risk of converging to
-    a local minimum in the optimization landscape.
+    This next line sets the batch size, which I've prefired to set as a function of the number of batches that a dataset provides us. I've always procceed to prime factorize the "len(dataset_train)" quantity and try to set
+    the number of batches as the integer divisor of "len(dataset_train)" that lies as close as possible to 1000. This is due to the appearance of negative loss values, in some training epochs, when I use a very high number
+    of batches. The other issue that prevents me from using a batch size much larger than 1000 is the possibility of having bins with a negative number of events in the final histogram of the scores: this effect is due to
+    the presence of events with negative weights in the 2018 MC. At the same time if I use a number of batches much smaller than 1000 I suffer from the problem of impairing the generalization capabilities of the model as
+    well as taking the risk of converging to a local minimum in the optimization landscape.
     '''
     miniBatch = int(round(len(dataset_train)/2000))  
     
